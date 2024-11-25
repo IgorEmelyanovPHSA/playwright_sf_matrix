55c081b745b506b16309b382d760689a7678ac2f
IgorEmelyanovPHSA <igor.emelyanov@phsa.ca>
2024-11-25 11:18:23 -0800

    UATX: after Refresh 25Nov.2024
    no Active History yet.

--- tests/UATX_Attachments_SF/UATX_SF_Case_Email_Phone_updates_as_811.spec.ts
+++ tests/UATX_Attachments_SF/UATX_SF_Case_Email_Phone_updates_as_811.spec.ts
@@ -8,7 +8,7 @@ test('Can_do_Updates_GORDON_KRULL_Email_Phone_as_811_in_SF', async ({page, brows
   await page.getByLabel('Username').fill('automation.811@phsa.ca.hlthbcuatx');
   await page.getByLabel('Password').click();
   await page.getByLabel('Password').fill('Technology1990!!!!!!');
-  /*
+  
   await page.getByRole('button', { name: 'Log In to Sandbox' }).click();
   await page.getByLabel('Search', { exact: true }).click();
   await page.waitForTimeout(2000);
@@ -35,11 +35,17 @@ test('Can_do_Updates_GORDON_KRULL_Email_Phone_as_811_in_SF', async ({page, brows
   await page.getByLabel('Telephone Number').nth(0).click();
   await page.waitForTimeout(1000);
   await page.getByLabel('Telephone Number').nth(0).fill('(250) 555-7777');
+  await page.locator('label').filter({ hasText: 'An address change for this' }).locator('span').first().click();
   await page.getByRole('button', { name: 'Save' }).click();
+  await page.waitForTimeout(5000);
   //Validate updated fields
-  const isTextVisible1 = await page.locator('text=igor.emelyanov_updates_sf@phsa.ca').isVisible();
-  const isTextVisible2 = await page.locator('text=2505557777').isVisible();
-*/  
+  const caseEmail = page.locator('a[href="mailto:igor.emelyanov_updates_sf@phsa.ca"]');
+  await expect(caseEmail).toBeVisible();
+  await expect(caseEmail).toHaveText('igor.emelyanov_updates_sf@phsa.ca');
+  const caseMobile = page.locator('text=2505557777');
+  //const caseMobile = page.locator('div.vloc-min-height div');
+  await expect(caseMobile).toBeVisible();
+  await expect(caseMobile).toHaveText('2505557777');
 }); 
 
 
